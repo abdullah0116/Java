732e1d1166dd34d7debc228faae7572b406319c3
abdullahkareem <akasak56@gmail.com>
2018-03-12 20:53:44 -0600

    Starting code for game snake in java

---
+++ Snake/Snake.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.geom.GeneralPath;
+  
+public class Snake {
+  public static void main(String[] args) {
+    Frame f = new Frame();
+    f.setVisible(true);// because by defualt its hidden
+    f.setSize(600,600);// setting the size
+    f.setTitle("Snake");
+    f.setBackground(Color.black);
+  }
+}
